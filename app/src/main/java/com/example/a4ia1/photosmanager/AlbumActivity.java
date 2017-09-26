79709b901df88121a400efd72a29cabd2d739d64
vmois <vladyslav.moisieienkov@gmail.com>
2017-09-26 22:24:33 +0200

    Add basic image view

--- app/src/main/java/com/example/a4ia1/photosmanager/AlbumActivity.java
+++ app/src/main/java/com/example/a4ia1/photosmanager/AlbumActivity.java
@@ -1,11 +1,16 @@
 package com.example.a4ia1.photosmanager;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.File;
@@ -17,15 +22,27 @@ public class AlbumActivity extends AppCompatActivity {
     private File currentFolder;
     private ImageView deleteFolderButton;
     private List<File> picturesList;
+    private LinearLayout imagesLayout;
+    private LinearLayout.LayoutParams lparams;
+    private LinearLayout childLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
 
+        // hide top bar
+        getSupportActionBar().hide();
+
         Bundle bundle = getIntent().getExtras();
         String folderName = bundle.getString("folderName").toString();
+        imagesLayout = (LinearLayout) findViewById(R.id.images_layout);
         picturesList = new ArrayList<>();
+
+        // get display size
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
 
         File pictureFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         String mainFolderName = getString(R.string.main_folder_name);
@@ -42,6 +59,39 @@ public class AlbumActivity extends AppCompatActivity {
                     picturesList.add(file);
                 }
             }
+        }
+        // initial childLayout set
+        int baseHeight = size.y / 4;
+        lparams = new LinearLayout.LayoutParams(size.x, baseHeight);
+        childLayout = new LinearLayout(getApplicationContext());
+        childLayout.setOrientation(LinearLayout.HORIZONTAL);
+        childLayout.setLayoutParams(lparams);
+
+        int picturesCount = 1;
+        // for properly image structure
+        Boolean leftIsSmaller = true;
+        for (File file: picturesList) {
+            Bitmap betterImage = betterImageDecode(file.getAbsolutePath());
+            ImageView iv = new ImageView(this);
+            iv.setImageBitmap(betterImage);
+            iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            if (leftIsSmaller) {
+                iv.setLayoutParams(new LinearLayout.LayoutParams(baseHeight, LinearLayout.LayoutParams.MATCH_PARENT, 1));
+            } else {
+                iv.setLayoutParams(new LinearLayout.LayoutParams(baseHeight, LinearLayout.LayoutParams.MATCH_PARENT, 2));
+            }
+            leftIsSmaller = !leftIsSmaller;
+            childLayout.addView(iv);
+            if (picturesCount % 2 == 0) {
+                leftIsSmaller = !leftIsSmaller;
+                imagesLayout.addView(childLayout);
+
+                // create new layout to prevent bug
+                childLayout = new LinearLayout(getApplicationContext());
+                childLayout.setOrientation(LinearLayout.HORIZONTAL);
+                childLayout.setLayoutParams(lparams);
+            }
+            picturesCount++;
         }
 
         deleteFolderButton = (ImageView) findViewById(R.id.delete_folder_icon);
@@ -64,5 +114,13 @@ public class AlbumActivity extends AppCompatActivity {
         currentFolder.delete();
         Toast.makeText(AlbumActivity.this, alertDeletedFolder, Toast.LENGTH_SHORT).show();
         AlbumActivity.this.finish();
+    }
+
+    private Bitmap betterImageDecode(String filePath) {
+        Bitmap myBitmap;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+        myBitmap = BitmapFactory.decodeFile(filePath, options);
+        return myBitmap;
     }
 }
