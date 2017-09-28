100d7795d122c9fa524c93b323fdad7679cdbbd7
vmois <vladyslav.moisieienkov@gmail.com>
2017-09-28 15:02:17 +0200

    Update camera activity and manifest

--- app/src/main/java/com/example/a4ia1/photosmanager/CameraActivity.java
+++ app/src/main/java/com/example/a4ia1/photosmanager/CameraActivity.java
@@ -2,8 +2,15 @@ package com.example.a4ia1.photosmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.Camera;
+import android.widget.FrameLayout;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private Camera camera;
+    private int cameraId = -1;
+    // private CameraPreview _cameraPreview;
+    private FrameLayout cameraFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
