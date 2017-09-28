5d880e057438182872706e54aa410482998e9b83
vmois <vladyslav.moisieienkov@gmail.com>
2017-09-28 15:01:50 +0200

    Create custom camera class

---
+++ app/src/main/java/com/example/a4ia1/photosmanager/CameraPreview.java
@@ -0,0 +1,49 @@
+package com.example.a4ia1.photosmanager;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * Created by 4ia1 on 2017-09-28.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+
+    public CameraPreview(Context context, Camera Camera) {
+        super(context);
+        this.camera = Camera;
+        this.surfaceHolder = this.getHolder();
+        this.surfaceHolder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
