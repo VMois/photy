4ee2420b4a9be302c410d067af6bd8c48f98fabf
vmois <vladyslav.moisieienkov@gmail.com>
2017-10-05 13:36:43 +0200

    Add Note class for notes data

---
+++ app/src/main/java/com/example/a4ia1/photosmanager/Helpers/Note.java
@@ -0,0 +1,18 @@
+package com.example.a4ia1.photosmanager.Helpers;
+
+/**
+ * Created by 4ia1 on 2017-10-05.
+ */
+public class Note {
+    private String imagePath;
+    private String title;
+    private String text;
+    private String color;
+
+    public Note(String ImagePath, String Title, String Text, String Color) {
+        imagePath = ImagePath;
+        title = Title;
+        text = Text;
+        color = Color;
+    }
+}
