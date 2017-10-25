f5936b99e0731a49c3cd5344ecefce2463bf5833
vmois <vladyslav.moisieienkov@gmail.com>
2017-10-25 16:39:41 +0200

    Add id to edittext and create main vers in NodeEditActivity

--- app/src/main/java/com/example/a4ia1/photosmanager/Activities/NoteEditActivity.java
+++ app/src/main/java/com/example/a4ia1/photosmanager/Activities/NoteEditActivity.java
@@ -2,6 +2,9 @@ package com.example.a4ia1.photosmanager.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.a4ia1.photosmanager.Helpers.Constants;
 import com.example.a4ia1.photosmanager.Helpers.DatabaseManager;
@@ -10,6 +13,9 @@ import com.example.a4ia1.photosmanager.R;
 public class NoteEditActivity extends AppCompatActivity {
 
     private DatabaseManager db;
+    private Button saveButton;
+    private EditText titleEditText;
+    private EditText textEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +29,19 @@ public class NoteEditActivity extends AppCompatActivity {
                 null,
                 1
         );
+        titleEditText = (EditText) findViewById(R.id.note_edit_title);
+        textEditText = (EditText) findViewById(R.id.note_edit_text);
+
+        saveButton = (Button) findViewById(R.id.note_edit_save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSaveButtonClick(v);
+            }
+        });
+    }
+
+    private void onSaveButtonClick(View v) {
+
     }
 }
