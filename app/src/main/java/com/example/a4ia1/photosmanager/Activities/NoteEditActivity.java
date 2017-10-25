8322dd49e4f0b483883d833121503483c07d9838
vmois <vladyslav.moisieienkov@gmail.com>
2017-10-25 17:29:11 +0200

    Add simple working edit feature without color change

--- app/src/main/java/com/example/a4ia1/photosmanager/Activities/NoteEditActivity.java
+++ app/src/main/java/com/example/a4ia1/photosmanager/Activities/NoteEditActivity.java
@@ -1,5 +1,7 @@
 package com.example.a4ia1.photosmanager.Activities;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.EditText;
 
 import com.example.a4ia1.photosmanager.Helpers.Constants;
 import com.example.a4ia1.photosmanager.Helpers.DatabaseManager;
+import com.example.a4ia1.photosmanager.Helpers.Note;
 import com.example.a4ia1.photosmanager.R;
 
 public class NoteEditActivity extends AppCompatActivity {
@@ -16,6 +19,8 @@ public class NoteEditActivity extends AppCompatActivity {
     private Button saveButton;
     private EditText titleEditText;
     private EditText textEditText;
+    private Note currentNote;
+    private String currentColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +34,21 @@ public class NoteEditActivity extends AppCompatActivity {
                 null,
                 1
         );
+
+        currentNote = db.getNoteById(noteId);
+        // check if note is found
+        // if not throw exception
+        if (currentNote.getId() == -1) {
+            throw new RuntimeException("Note with given ID not found!");
+        }
+        currentColor = currentNote.getColor();
         titleEditText = (EditText) findViewById(R.id.note_edit_title);
+        titleEditText.setText(currentNote.getTitle());
+        titleEditText.setTextColor(Color.parseColor(currentColor));
+
         textEditText = (EditText) findViewById(R.id.note_edit_text);
+        textEditText.setText(currentNote.getText());
+        textEditText.setTextColor(Color.parseColor(currentColor));
 
         saveButton = (Button) findViewById(R.id.note_edit_save_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +60,17 @@ public class NoteEditActivity extends AppCompatActivity {
     }
 
     private void onSaveButtonClick(View v) {
-
+        String title = titleEditText.getText().toString();
+        String text = textEditText.getText().toString();
+        Note newNote = new Note(
+                currentNote.getId(),
+                title,
+                text,
+                currentColor,
+                currentNote.getImagePath()
+        );
+        db.updateNote(newNote);
+        Intent intent = new Intent(getApplicationContext(), NotesActivity.class);
+        startActivity(intent);
     }
 }
