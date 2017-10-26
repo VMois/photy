439ab982a499baccfca12d932ac6df10766f51fd
vmois <vladyslav.moisieienkov@gmail.com>
2017-10-26 13:54:51 +0200

    Add color change support in note edit activity

--- app/src/main/java/com/example/a4ia1/photosmanager/Activities/NoteEditActivity.java
+++ app/src/main/java/com/example/a4ia1/photosmanager/Activities/NoteEditActivity.java
@@ -1,12 +1,13 @@
 package com.example.a4ia1.photosmanager.Activities;
 
-import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.a4ia1.photosmanager.Helpers.Constants;
 import com.example.a4ia1.photosmanager.Helpers.DatabaseManager;
@@ -16,11 +17,12 @@ import com.example.a4ia1.photosmanager.R;
 public class NoteEditActivity extends AppCompatActivity {
 
     private DatabaseManager db;
-    private Button saveButton;
     private EditText titleEditText;
     private EditText textEditText;
     private Note currentNote;
     private String currentColor;
+    private TextView titleTextView;
+    private TextView textTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +44,49 @@ public class NoteEditActivity extends AppCompatActivity {
             throw new RuntimeException("Note with given ID not found!");
         }
         currentColor = currentNote.getColor();
+
         titleEditText = (EditText) findViewById(R.id.note_edit_title);
         titleEditText.setText(currentNote.getTitle());
-        titleEditText.setTextColor(Color.parseColor(currentColor));
 
         textEditText = (EditText) findViewById(R.id.note_edit_text);
         textEditText.setText(currentNote.getText());
-        textEditText.setTextColor(Color.parseColor(currentColor));
 
-        saveButton = (Button) findViewById(R.id.note_edit_save_button);
+        titleTextView = (TextView) findViewById(R.id.title_textview);
+        textTextView = (TextView) findViewById(R.id.text_textview);
+
+        Button btRedColor = (Button) findViewById(R.id.noteRedButton);
+        Button btBlueColor = (Button) findViewById(R.id.noteBlueButton);
+        Button btGreenColor = (Button) findViewById(R.id.noteGreenButton);
+
+        btRedColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onColorChoose(v);
+            }
+        });
+        btBlueColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onColorChoose(v);
+            }
+        });
+        btGreenColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onColorChoose(v);
+            }
+        });
+
+        Button saveButton = (Button) findViewById(R.id.note_edit_save_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onSaveButtonClick(v);
             }
         });
+
+        // set color on init
+        setColorForEdit(Color.parseColor(currentColor));
     }
 
     private void onSaveButtonClick(View v) {
@@ -71,5 +101,20 @@ public class NoteEditActivity extends AppCompatActivity {
         );
         db.updateNote(newNote);
         this.finish();
+    }
+
+    private void onColorChoose(View view) {
+        int intColor = ((ColorDrawable) view.getBackground()).getColor();
+        setColorForEdit(intColor);
+    }
+
+    private void setColorForEdit(int color) {
+        // update color of text to inform user
+        titleEditText.setTextColor(color);
+        titleTextView.setTextColor(color);
+        textTextView.setTextColor(color);
+
+        // convert color from int format to HEX -> to save in db
+        currentColor = String.format("#%06X", (0xFFFFFF & color));
     }
 }
